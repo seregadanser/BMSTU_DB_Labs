858904db4971f3b669c26632d70c359a41487c8d
Sergey <kalashnikov.sergey01@gmail.com>
2022-11-22 23:43:16 +0300

    up

--- Lab5/4.sql
+++ Lab5/4.sql
@@ -7,27 +7,17 @@ use Lab1
 go
 
 EXEC xp_cmdshell 'bcp "SELECT * FROM [Lab1].[dbo].[Computer] FOR XML Auto" queryout "D:\Study\DataBase\Lab5\t2.xml" -T -c -r'
-if object_id('ComputerCopy') is not null
-	drop table ComputerCopy
-GO
-create table ComputerCopy
-(
-    MemoryValue int,
-	Furency int,
-	CoolType varchar(100),
-	NumberOfCores int,
-    Id int not null identity
-);
-GO
 
 DECLARE @XmlFile XML
 SELECT @XmlFile = BulkColumn
-FROM  OPENROWSET(bulk 'D:\Labs\DataBase\Lab5\t2.xml',  SINGLE_BLOB) x;
+FROM  OPENROWSET(bulk 'D:\Labs\DataBase\Lab5\t2.xml',  SINGLE_BLOB) as x;
 
-insert into ComputerCopy(MemoryValue, Furency, CoolType, NumberOfCores)
+
 select Mem = xxm.value('@MemoryValue', 'int'), Fu = xxm.value('@Furency', 'int'),
 Co = xxm.value('@CoolType', 'varchar(100)'), Nu = xxm.value('@NumberOfCores', 'int')
 from @XmlFile.nodes('Lab1.dbo.Computer') as XTbl(xxm)
-select * from ComputerCopy
 
-drop table ComputerCopy
+select Mem = xxm.value('@MemoryValue', 'int'), Fu = xxm.value('@Furency', 'int'),
+Co = xxm.value('@CoolType', 'varchar(100)'), Nu = xxm.value('@NumberOfCores', 'int')
+from @XmlFile.nodes('Lab1.dbo.Computer') as XTbl(xxm) where xxm.value('@NumberOfCores', 'int') = 1
+
